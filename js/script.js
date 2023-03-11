b8e767ee30393a26b1288e793e4ae14546ab59e9
yutaokkots <yuta.okkotsu@gmail.com>
2023-03-10 22:26:29 -0700

    currently not running, but library being built

--- js/script.js
+++ js/script.js
@@ -3,5 +3,29 @@ console.log("initiated")
 
 
 
-const mainElement = document.createElement("body")
-const divElement= document.createElement("div")
+
+const QUESTIONS_PYTHON = {
+    1:  {Q:"An array is to an object, as list is to a(n)_______.",                                  A:"dictionary",B: "reference", C:"glossary", D:"object", E:"array", F:"microarray"},
+    2:  {Q:"A template literal is to Javascript as _______ is to Python.",                          A:"f-string", B: "g-string", C:"template-literal", D:"string-literal", E:"", F:""},
+    3:  {Q:"This symbol or operator represents a comment in JS, and a floor division in Python",    A:"//", B:"@@", C:"**", D:"##"},
+    4:  {Q:"A ______ returns the remainder of a division operation",                                A:"%", B:"/?", C:"&|", D:"$!"},
+    5:  {Q:"In order to iterate with a key:index pair, the ______ function may be used in Python",  A:"enumerate()", B:"itemize()", C:"index()", D:"print()"},
+    6:  {Q:"What is the name of the immutable data type out of the three listed here?: {'1':'2'} or (1,2) or [1,2]", A:"tuple", B:"buccal", C:"ducal", D:"nuptial", E:"list", F:"dictionary"},
+    8:  {Q:"When creating a Class, the following command is used to 'construct' the class",         A:"__init__", B:"constructor()", C:"initializer()", D:"__starter__", E:"def"},
+    9:  {Q:"This referential key is used inside a Class in Python",                                 A:"self", B:"this", C:"my", D:"at", E:"from"},
+    10: {Q:"What is the result of this conditional statement in Python?: if 2 == '2'", A:"False", B:"True", C:"falsey", D:"truthy", E:"undefined", },
+    11: {Q:"Space or tab?", A:"space", B:"tab"},
+    12: {Q:"Variables in Python are written in ______-case", A:"snake", B:"camel", C:"lisp", D:"flat"},
+    13: {Q:"Which of the following is not a primitive data type in Python (ex. BigInt does not exist in Python)", A:"null", B:"int", C:"str", D:"float", E:"bool"},
+    14: {Q:"In conditional statements, which of the following is not a valid command?", A:"else if", B:"elif", C:"else", D:"if"},
+    15: {Q:"", A:"", B:"", C:"", D:""},
+    16: {Q:"", A:"", B:"", C:"", D:""},
+    17: {Q:"", A:"", B:"", C:"", D:""}
+}
+
+const QUETIONS_INDIA = {
+
+}
+
+
+const mainElement = document.querySelectorAll("main")
