8381d1d3a5c146e27fe49128ddbf3fb0d028bd40
yutaokkots <yuta.okkotsu@gmail.com>
2023-03-10 23:10:28 -0700

    updated script.js to include outline of functions needed

---
+++ js/quizzes.js
@@ -0,0 +1,17 @@
+// -- constants -- //
+export const QUESTIONS_PYTHON = {
+  0:  ["An array is to an object, as list is to a(n)_______.", ["dictionary","reference","glossary","object","array","microarray"], 0],
+  1:  ["A template literal is to Javascript as _______ is to Python.", ["f-string", "g-string","template-literal","string-literal","string-template","variable-template"], 0],
+  2:  ["This symbol or operator represents a comment in JS, and a floor division in Python", ["//","@@", "**", "##","::",".."], 0],
+  3:  ["A ______ returns the remainder of a division operation", ["%","/?","&|","$!","$","&"], 0],
+  4:  ["In order to iterate with a key:index pair, the ______ function may be used in Python", ["enumerate()","itemize()","index()","print()","for...in"], 0],
+  5:  ["What is the name of the immutable data type out of the three listed here?: {'1':'2'} or (1,2) or [1,2]", ["tuple","buccal","ducal","nuptial","list","dictionary"], 0],
+  6:  ["When creating a Class, the following command is used to 'construct' the class", ["__init__","constructor()","initializer()","__starter__","def"], 0],
+  8:  ["This referential key is used inside a Class in Python",["self","this","my","at","from"], 0],
+  9:  ["What is the result of this conditional statement in Python?: if 2 == '2':",["False","True","falsey","truthy","undefined","null"], 0],
+  10: ["Space or tab?", ["space","tab","space-ish","tab-ish"], 1],
+  11: ["Variables in Python are written in ______-case",["snake","camel","lisp","flat","horse","worm"], 0],
+  12: ["Which of the following is not a primitive data type in Python (ex. BigInt does not exist in Python)",["null","int","str","float","bool"], 0],
+  13: ["In conditional statements, which of the following is not a valid command?",["else if","elif","else","if"], 0],
+}
+
